2a404aef27497ca8bac2b3562ab06a85870ea766
padnitri <57793978+padnitri@users.noreply.github.com>
2021-08-07 14:34:53 +0700

    Repush to trigger

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web - Test Edit 03 - Netlify
+      SIT Subject Reservation Web - Test Edit 04 - Repush-Netlify
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
